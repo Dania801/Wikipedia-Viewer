e4888b54b6938f2d471e8fec0b3a3e492cc1ed10
Dania801 <ssansnnom07@gmail.com>
2017-05-18 21:20:58 +0300

    'I feel luck' button is done , in addition to the UI .

--- Viewer_code.js
+++ Viewer_code.js
@@ -6,13 +6,14 @@ function getKeyInfo(key){
   var wikiQuery = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + key + '&prop=info&inprop=url&utf8=&format=json' ;
 
   var currDiv = document.getElementById('body') ;
+  currDiv.style.backgroundColor = "#E1F5FE" ;
   currDiv.innerHTML =  '<div id = "">'+
         '<div class="container contailer-fluid" id="search-bar">'+
           '<div class="col-xs-1"><img src="search.svg" alt="image" id="min-pic"/></div>'+
           '<div class="input-group" style="width: 400px; margin-top: 35px ; ">'+
             '<input type="text" class="form-control" id="search">'+
             '<span class="input-group-btn">'+
-              '<button class="btn btn-default" type="button" id="search-bar-btn" onclick="getKey()">Go!</button>'+
+              '<button class="btn btn-default" type="button" id="search-bar-btn" onclick="getKey()" style="font-weight:bold">Go!</button>'+
             '</span>'+
           '</div>'+
         '</div>'+
@@ -20,7 +21,7 @@ function getKeyInfo(key){
         '</div>'+
       '</div> ';
 
-   document.getElementById('search').value = " "+key ;
+   document.getElementById('search').value = key ;
   $.ajax({
     url: wikiQuery ,
     dataType : "jsonp",
@@ -31,8 +32,9 @@ function getKeyInfo(key){
       {
         var title = response.query.search[i].title ;
         var url = title.replace(/ /g, "_");
-        div.innerHTML = div.innerHTML + '<div class="jumbotron container" style="background-color: transparent;text-align:left;height:100px ;"><span id="title">'+title
-                      +'</span><br/><a href="https://en.wikipedia.org/wiki/'+url
+        var fullURL = "https://en.wikipedia.org/wiki/"+url ;
+        div.innerHTML = div.innerHTML + '<div class="jumbotron container" style="background-color: transparent;text-align:left;height:100px ;"><span id="title"><a href = "'+fullURL+'">'+title
+                      +'</a></span><br/><a href="https://en.wikipedia.org/wiki/'+url
                       +'"target="_blank">'+'https://en.wikipedia.org/wiki/'+url
                       +'</a></br><div style="width:500px;">'+response.query.search[1].snippet+'</div></div>';
       }
